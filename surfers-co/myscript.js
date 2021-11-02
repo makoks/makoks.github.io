aba4c9f635290d3e0d86c84477cb76bc067a7864
makoks <kolesoks@gmail.com>
2021-11-02 20:13:55 +0500

    feat: change to russian

--- surfers-co/myscript.js
+++ surfers-co/myscript.js
@@ -1,11 +1,3 @@
-function showCircle() {
-    document.getElementById("circle").style.display = "block";
-}
-
-function hideCircle() {
-    document.getElementById("circle").style.display = "none";
-}
-
 function teamActive(element) {
     element.classList.add("tcard-active"); 
     element.children[3].src = "images/Waves.png";
@@ -22,4 +14,53 @@ function blogHover(element) {
 
 function blogUsual(element) {
     element.classList.remove("blog-hover");
+}
+
+function changeBoardImage(element) {
+	let imgTabs = element.parentNode.getElementsByClassName("img-tab");
+	for (i = 0; i < imgTabs.length; i++) {
+		imgTabs[i].classList.remove("img-tab-active"); 
+	}
+	element.parentNode.parentNode.getElementsByClassName("board")[0].src = element.src;
+	element.classList.add("img-tab-active");
+}
+
+function changeTab(element, contentName) {
+	let tabs = element.parentNode.getElementsByTagName("a");
+	for (i = 0; i < tabs.length; i++) {
+		tabs[i].classList.remove("active"); 
+	}
+	let content = element.parentNode.parentNode.getElementsByClassName("tab-content")[0].getElementsByTagName("p");
+	for (i = 0; i < content.length; i++) {
+		content[i].classList.remove("tab-content-show"); 
+	}
+	let contentShow = element.parentNode.parentNode.getElementsByClassName("tab-content")[0].getElementsByClassName(contentName);
+	for (i = 0; i < contentShow.length; i++) {
+		contentShow[i].classList.add("tab-content-show"); 
+	}
+	element.classList.add("active");
+}
+
+function nextSlide() {
+	let currentSlide = document.getElementById("counter").innerHTML[0];
+	document.getElementsByClassName("slide-show")[0].classList.remove("slide-show");
+	if (currentSlide == 5) {
+		document.getElementById("counter").innerHTML = 1 + "/5";
+		document.getElementsByClassName("slide1")[0].classList.add("slide-show");
+	} else {
+		document.getElementById("counter").innerHTML = (parseInt(currentSlide) + 1) + "/5";
+		document.getElementsByClassName("slide" + ((parseInt(currentSlide) + 1)))[0].classList.add("slide-show");
+	}
+}
+
+function prevSlide() {
+	let currentSlide = document.getElementById("counter").innerHTML[0];
+	document.getElementsByClassName("slide-show")[0].classList.remove("slide-show");
+	if (currentSlide == 1) {
+		document.getElementById("counter").innerHTML = 5 + "/5";
+		document.getElementsByClassName("slide5")[0].classList.add("slide-show");
+	} else {
+		document.getElementById("counter").innerHTML = (parseInt(currentSlide) - 1) + "/5";
+		document.getElementsByClassName("slide" + ((parseInt(currentSlide) - 1)))[0].classList.add("slide-show");
+	}
 }
